3a8d0ebc8ecb393dc9672d9f5148cd7eaafe8676
yogita-s-24 <yogitashete24@gmail.com>
2023-10-19 14:28:26 +0530

    read all data from array

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ import express from "express";
 
 const app = express();
 
-const PORT = 5000;
+app.use(express.json());
+
+const student = [];
+
+// defined a get req routes at '/student' to read all students from the array
+app.get('/students',(req,res)=>{
+    res.json({
+        sucess:true,
+        student:student,
+        message:"students array sucessfully"
+
+    })
+})
 
 app.get('/health',(req,res)=>{
     res.send({status:"OK hai"});
 })
+
+const PORT = 5000;
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
