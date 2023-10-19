624f606a26ad0555b7580397b36b87c712d33273
yogita-s-24 <yogitashete24@gmail.com>
2023-10-19 18:07:42 +0530

    post method suceessfully working

--- index.js
+++ index.js
@@ -4,24 +4,41 @@ const app = express();
 
 app.use(express.json());
 
-const student = [];
+const students=[];
 
-// defined a get req routes at '/student' to read all students from the array
-app.get('/students',(req,res)=>{
+app.post("/students",(req,res)=>{
+
+    const id = Math.floor(Math.random() * 1000) + 1;
+
+    const {name, age, email, mobile} = req.body;
+
+    const newStudents = {
+        id,
+        name,
+        age,
+        email,
+        mobile
+    }
+
+    students.push(newStudents);
+
     res.json({
-        sucess:true,
-        student:student,
-        message:"students array sucessfully"
-
-    })
+        status:"success",
+        students : students,
+        message: 'Student added successfully'
+    });
 })
 
-app.get('/health',(req,res)=>{
-    res.send({status:"OK hai"});
+
+
+app.get('/home',(req,res)=>{
+    res.send("Home page here");
 })
 
 const PORT = 5000;
 
 app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 })
+
+
