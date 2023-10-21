40423d0d75080f5daaab0c9c6ee88ac5ce8109b1
yogita-s-24 <yogitashete24@gmail.com>
2023-10-21 15:47:53 +0530

    updated specific stud data api working successfully

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(express.json());
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectMongoDB = async () => {
-    const conn = await mongoose.connect(MONGODB_URI);
+  const conn = await mongoose.connect(MONGODB_URI);
 
-    if (conn) {
-        console.log("Connected to MongoDB Successfully...");
-    }
+  if (conn) {
+    console.log("Connected to MongoDB Successfully...");
+  }
 };
 
 connectMongoDB();
@@ -25,85 +25,118 @@ connectMongoDB();
 
 //for sample checking
 app.get("/health", (req, res) => {
-    res.send("I'm healthy");
+  res.send("I'm healthy");
 });
 
 //post API for pas student data
 app.post("/students", async (req, res) => {
-    const { name, age, email, mobile } = req.body;
+  const { name, age, email, mobile } = req.body;
 
-    const newStudent = new Student({
-        name: name,
-        age: age,
-        mobile: mobile,
-        email: email,
-    });
-    const saveStudent = await newStudent.save();
-    res.json({
-        status: "success",
-        students: saveStudent,
-        message: "Student added successfully",
-    });
+  const newStudent = new Student({
+    name: name,
+    age: age,
+    mobile: mobile,
+    email: email,
+  });
+  const saveStudent = await newStudent.save();
+  res.json({
+    status: "success",
+    students: saveStudent,
+    message: "Student added successfully",
+  });
 });
 
 //get API for find student using email
 app.get("/student", async (req, res) => {
-    const { email } = req.query;
-    const student = await Student.findOne({ email: email });
-    res.json({
-        status: "success",
-        data: student,
-        message: "Student fatch successfully",
-    });
+  const { email } = req.query;
+  const student = await Student.findOne({ email: email });
+  res.json({
+    status: "success",
+    data: student,
+    message: "Student fatch successfully",
+  });
 });
 
 //get API for find all student at a time
 app.get("/students", async (req, res) => {
-    // console.log('students',students);
-    const students = await Student.find();
+  // console.log('students',students);
+  const students = await Student.find();
 
-    res.json({
-        status: "success",
-        data: students,
-        message: "All Students fetched successfully",
-    });
+  res.json({
+    status: "success",
+    data: students,
+    message: "All Students fetched successfully",
+  });
 });
 
 //delete api for delete student from db
 app.delete("/student/:_id", async (req, res) => {
-    const { _id } = req.params;
+  const { _id } = req.params;
 
-    await Student.deleteOne({ _id: _id });
+  await Student.deleteOne({ _id: _id });
 
-    res.json({
-        success: true,
-        message: `Student deleted Successfully ${_id}`,
-    });
+  res.json({
+    success: true,
+    message: `Student deleted Successfully ${_id}`,
+  });
 });
 
 //update api for update student data from db using `put` method
 //put is used to update a resources (all data)
 app.put("/student/:_id", async (req, res) => {
-    const {_id} = req.params;
-    const { name, age, email, mobile } = req.body;
+  const { _id } = req.params;
+  const { name, age, email, mobile } = req.body;
 
-    await Student.updateOne(
-       { _id:_id }, { $set: { name: name, age: age, email: email, mobile: mobile } }
-    );
+  await Student.updateOne(
+    { _id: _id },
+    { $set: { name: name, age: age, email: email, mobile: mobile } }
+  );
 
-    const findStudent = await Student.findOne({_id:_id});
+  const findStudent = await Student.findOne({ _id: _id });
 
-
-    res.json({
-        success: true,
-        data : findStudent,
-        message: `Student updated Successfully with id :${_id}`
-    });
+  res.json({
+    success: true,
+    data: findStudent,
+    message: `Student updated Successfully with id :${_id}`,
+  });
 });
 
+//update specific data
+//patch is used to update a piece of data
+app.patch("/student/:_id", async (req, res) => {
+  const { _id } = req.params;
+  let { name, age, email, mobile } = req.body;
+
+  const student = await Student.findById(_id);
+
+  if (name) {
+    student.name = name;
+  }
+
+  if (age) {
+    student.age = age;
+  }
+
+  if (email) {
+    student.email = email;
+  }
+
+  if (mobile) {
+    student.mobile = mobile;
+  }
+
+  //save the changes in database
+const updateSpecificData = await student.save();
+
+  res.json({
+    success: true,
+    data: updateSpecificData,
+    message: `Update Specific Data Successfuly With Id ${_id}`,
+  });
+});
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
